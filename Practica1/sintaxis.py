bb014035883e628c62ea8eebc5a6792284bcf1e7
GabrielaMartinez20 <123047794@upq.edu.mx>
2025-05-16 18:19:44 -0600

    Variables

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -6,5 +6,26 @@ comentario de mas de 1 linea
 en python """
 
 #2. Strings
-print("hola soy una cadena")
-print(' soy la otraa')
+print('soy la otra')
+
+variable1 =  "hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3. VARIABLES
+x = "gabi"
+x = 4
+x = 5.90
+print(x)
+
+x = int(3)
+y = float(3)
+z = str(3)
+
+print(x,y,z)
+#Por si se olvida el tipo de dato que le coreesponde
+print(type(x))
+print(type(y))
+print(type(z))
