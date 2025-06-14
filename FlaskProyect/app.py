23a6502b8a19747641a7a363f50f612614cb0731
GabrielaMartinez20 <123047794@upq.edu.mx>
2025-06-13 18:40:37 -0600

    Conexión a MySQL con excepciones
    conexión a MySQL con la base de datos, con las posibles excepciones que pudiera haber en esta ruta.

--- FlaskProyect/app.py
+++ FlaskProyect/app.py
@@ -1,6 +1,27 @@
-from flask import Flask 
+from flask import Flask,jsonify #importaciones
+from flask_mysqldb import MySQL 
+import MySQLdb
 
 app= Flask(__name__)   
+
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']="12345"
+app.config['MYSQL_DB']="dbflask"
+#app.config['MYSQL_PORT']=3306 //USAR SOLO EN CAMBIO DE PUERTO
+
+mysql=MySQL(app)
+
+#Ruta para probar la conexion a MYSQL
+@app.route('/DBCheck')
+def DB_check():
+    try:
+        cursor=mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify( {'status':'ok','message':'Conectado con exito'} ),200
+    except MySQLdb.MySQLError as e:
+        return jsonify( {'status':'error','message':str(e)} ),500
+        
 
 #Asignar ruta simple
 @app.route('/')
